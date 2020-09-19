a25014347509cb734195d08f4f15a68a191134b3
RusscoJ <russcojrv1na@gmail.com>
2020-09-19 15:18:26 +0700

    6 files modified

--- pages/about/about.ts
+++ pages/about/about.ts
@@ -1,14 +1,27 @@
+about.ts
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
-  templateUrl: 'about.html'
+  templateUrl: 'about.html',
+  styleUrls: ['about.css'], 
 })
 export class AboutPage {
+public tuitionFeeAmount: number;
+public paymentTerms: number;
+public totalInterestPayable: number; 
+public totalAmount: number;
+public monthlyPayment: number;
 
   constructor(public navCtrl: NavController) {
 
   }
+
+solve() {
+  this.totalInterestPayable=(this.tuitionFeeAmount * 0.0075) * this.paymentTerms;
+  this.totalAmount= parseInt(this.tuitionFeeAmount) + parseInt(this.totalInterestPayable);
+  this.monthlyPayment= (this.totalAmount / this.paymentTerms);
+}
 
 }
